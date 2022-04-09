89e6891f577853a833db74a1711f87cd1d79808c
Zhiwu Mao <zwmao@outlook.com>
2022-04-09 14:36:13 +0800

    完善功能

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import imp
 import sys
 import time
+import json
 import DBService
 import requests
 from selenium import webdriver
@@ -21,8 +22,11 @@ class TwitterFollowers():
         countKeyField='FOLLOWERS_COUNT'
 
         last=self.dbService.getFollowers()
+        print(json.dumps(last))
         if countKeyField in last.keys():
             lastCount= int(last[countKeyField],10)
+
+        print(lastCount)
 
         nowCount=int(followersCount,10)
 
@@ -33,7 +37,7 @@ class TwitterFollowers():
         while i < 3:
             try:
                 time.sleep(0.5)
-                return  requests.get(url, timeout=5,headers=self.headers)
+                return  requests.get(url, timeout=5)
             except requests.exceptions.RequestException:
                 print(url)
                 print("[请求超时]:第"+str(i+1)+"次请求超时！即将进行第"+str(i+2)+"次尝试！")
@@ -53,20 +57,23 @@ class TwitterFollowers():
 
             followers = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/div/div/div[5]/div[2]/a/span[1]/span')
             followersCountText=followers.text
-            #followersCountText="6,998" 
+            #followersCountText="7,000"  测试用
             followersCount=self.RemoveFormat(followersCountText)
-            followersCountChange=self.CalcCountChange(followersCount)
+            followersCountChange=str(self.CalcCountChange(followersCount))
             print(followersCountText)
 
             self.dbService.addTwitterFollowers(username,followersCount,followersCountText,followersCountChange)
             try:
                 remote=""
-                self.requestDeal(remote+"/add?username="+username+"&followersCount="+followersCount+"&followersCountText"+followersCountText+"&followersCountChange="+followersCountChange)
+                url=remote+"/add?username="+username+"&followersCount="+followersCount+"&followersCountText="+followersCountText+"&followersCountChange="+followersCountChange
+                print(url)
+                self.requestDeal(url)
             except Exception as error:
+                print(error)
                 pass
 
-            print('一个小时后，再次抓取~')
-            time.sleep(1*3600*1) #一个小时
+            print('半个小时后，再次抓取~')
+            time.sleep((1*3600*1)/2) #半个小时
 
 #主模块执行
 if __name__ == "__main__":
