36c3dbefa0416639b145b36f0aacb52b6275de21
Zhiwu Mao <zwmao@outlook.com>
2022-04-09 11:25:50 +0800

    完善统计逻辑，增加变化值和展示值

--- DBService.py
+++ DBService.py
@@ -17,13 +17,15 @@ class DBService():
         self.db.execute("CREATE TABLE IF NOT EXISTS T_TWITTER_FOLLOWERS_HISTORY(\
                                     USER_NAME VARCHAR(500),\
                                     FOLLOWERS_COUNT VARCHAR(500),\
+                                    FOLLOWERS_COUNT_TEXT VARCHAR(500),\
+                                    FOLLOWERS_COUNT_CHANGE VARCHAR(500),\
                                     IMP_DATE VARCHAR(10) DEFAULT (date('now')),\
                                     IMP_TIME VARCHAR(19) DEFAULT (datetime('now','localtime'))\
                         )")
         pass
 
-    def addTwitterFollowers(self, userId,followers):
-        self.db.execute("INSERT INTO T_TWITTER_FOLLOWERS_HISTORY(USER_NAME,FOLLOWERS_COUNT) VALUES(?,?)",(userId,followers))
+    def addTwitterFollowers(self, username,followersCount,followersCountText,followersCountChange):
+        self.db.execute("INSERT INTO T_TWITTER_FOLLOWERS_HISTORY(USER_NAME,FOLLOWERS_COUNT,FOLLOWERS_COUNT_TEXT,FOLLOWERS_COUNT_CHANGE) VALUES(?,?,?,?)",(username,followersCount,followersCountText,followersCountChange))
 
     def getFollowers(self):
         result=self.db.query("SELECT * FROM T_TWITTER_FOLLOWERS_HISTORY ORDER BY IMP_TIME DESC LIMIT 1")
@@ -32,7 +34,9 @@ class DBService():
             res= {
                 'USER_NAME':row[0],
                 'FOLLOWERS_COUNT':row[1],
-                'IMP_DATE':row[2],
-                'IMP_TIME':row[3],
+                'FOLLOWERS_COUNT_TEXT':row[2],
+                'FOLLOWERS_COUNT_CHANGE':row[3],
+                'IMP_DATE':row[4],
+                'IMP_TIME':row[5],
             }
         return res
--- server.py
+++ server.py
@@ -10,6 +10,22 @@ class TwitterFollowers():
         self.dbService=DBService.DBService()
         self.GetTwitterFollowers("")
 
+    def RemoveFormat(self,text):
+        return text.replace(',','')
+
+    def CalcCountChange(self,followersCount):
+        lastCount=0
+        nowCount=followersCount
+        countKeyField='FOLLOWERS_COUNT'
+
+        last=self.dbService.getFollowers()
+        if countKeyField in last.keys():
+            lastCount= int(last[countKeyField],10)
+
+        nowCount=int(followersCount,10)
+
+        return nowCount-lastCount
+
     def GetTwitterFollowers(self,username):
         while True:
             option = webdriver.ChromeOptions()
@@ -19,11 +35,15 @@ class TwitterFollowers():
 
             driver.get("https://twitter.com/"+username)
             followers = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/div/div/div[5]/div[2]/a/span[1]/span')
-            print(followers.text)
+            followersCountText=followers.text
+            #followersCountText="6,998" 
+            followersCount=self.RemoveFormat(followersCountText)
+            followersCountChange=self.CalcCountChange(followersCount)
+            print(followersCountText)
 
-            self.dbService.addTwitterFollowers(username,followers.text)
+            self.dbService.addTwitterFollowers(username,followersCount,followersCountText,followersCountChange)
             
-            time.sleep(1*3600*2) #两个小时
+            time.sleep(1*3600*1) #一个小时
 
 #主模块执行
 if __name__ == "__main__":
